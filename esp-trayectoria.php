eeb1e4a56e8f7f22dab004561bdde921fd872847
flatoveloz <cristianveraza@gmail.com>
2016-01-17 23:59:01 -0300

    Cambios varios

--- esp-trayectoria.php
+++ esp-trayectoria.php
@@ -2,11 +2,20 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 
-<title>LUIGI DE CARLO / HISTORIA</title>
+<title>LUIGI DE CARLO / TRAYECTORIA</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="css/supersized.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="theme/supersized.shutter.css" type="text/css" media="screen" />
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+<link rel="stylesheet" href="fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+<link rel="stylesheet" href="fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+<link rel="stylesheet" href="fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+<script type="text/javascript" src="fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+<script type="text/javascript" src="fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+<script type="text/javascript" src="fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+<script type="text/javascript" src="fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6"></script>
+<!--<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>-->
 <script type="text/javascript" src="js/jquery.easing.min.js"></script>
 <script type="text/javascript" src="js/supersized.3.2.7.min.js"></script>
 <script type="text/javascript" src="theme/supersized.shutter.min.js"></script>
@@ -24,8 +33,26 @@ slides 					:  	[			// Slideshow Images
 ]
 });
 });
+
+//Fancybox
+  $(document).ready(function() {
+    $(".fancybox").fancybox({
+      openEffect  : 'elastic',
+      closeEffect : 'elastic',
+      helpers : {
+        title : {
+          type : 'float'
+        }
+      }
+    })
+
+    });
+
+
+
 </script>
- <link href="css/ldc.css" rel="stylesheet" type="text/css" />
+
+<link href="css/ldc.css" rel="stylesheet" type="text/css" />
 <link href="fonts/stylesheet.css" rel="stylesheet" type="text/css" />
 <style type="text/css">
 body {
@@ -33,7 +60,7 @@ body {
 }
 </style>
 </head>
-	
+
 <body>
 <div id="framecontent">
 <?php include("inc/menu.php"); ?></div>
@@ -46,11 +73,13 @@ body {
     <td height="135px">&nbsp;</td>
   </tr>
   <tr>
-    <td bgcolor="#000"><table width="100%" border="0" cellspacing="0" cellpadding="30">
-      <tr>
-        <td class="titulo">HISTORIA</td>
-      </tr>
-    </table></td>
+    <td bgcolor="#000">
+      <table width="100%" border="0" cellspacing="0" cellpadding="30">
+        <tr>
+          <td class="titulo">TRAYECTORIA</td>
+        </tr>
+      </table>
+    </td>
   </tr>
   <tr>
     <td height="30">&nbsp;</td>
@@ -58,220 +87,28 @@ body {
   <tr>
     <td><table width="984" border="0" cellspacing="0" cellpadding="0">
       <tr>
-        <td width="656" valign="top" background="img/back.png"><table width="100%" border="0" cellspacing="0" cellpadding="30">
-          <tr>
-            <td class="texto"><table width="596" border="0" cellspacing="0" cellpadding="0">
-              <tr>
-                <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                    <tr>
-                    <td class="textogrande"><a href="prensa/p01.pdf" target="_blank" class="botontexto">›› NOTA DE PRENSA</a></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                        </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                        </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                        </tr>
-                      </table></td>
-                    </tr>
-                  <tr>
-                    <td class="textogrande"><a href="prensa/p02.pdf" target="_blank" class="botontexto">›› NOTA DE PRENSA</a></td>
-                    </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                        </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                        </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                        </tr>
-                      </table></td>
-                    </tr>
-                  <tr>
-                    <td class="textogrande"><a href="prensa/p03.pdf" target="_blank" class="botontexto">›› NOTA DE PRENSA</a></td>
-                    </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                      </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                    </table></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><a href="prensa/p04.pdf" target="_blank" class="botontexto">›› NOTA DE PRENSA</a></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                      </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                    </table></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande">
-                      <a href="prensa/p05.pdf" target="_blank" class="botontexto">›› NOTA DE PRENSA</a></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                      </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                    </table></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><a href="prensa/p06.pdf" target="_blank" class="botontexto">›› NOTA DE PRENSA</a></td>
-                  </tr>
-
-                  <tr>
-                    <td class="textogrande"><a href="historia/h07.pdf" target="_blank" class="botontexto">›› 02/12/59 CARTA DE AGRADECIMIENTO DE LA EMBAJADA DE EEUU</a></td>
-                    </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                        </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                        </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                        </tr>
-                      </table></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><a href="historia/h05.pdf" target="_blank" class="botontexto">›› 10/04/64 CARTA DE AGRADECIMIENTO DE LA EMBAJADA DE ESPAÑA</a></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                        </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                        </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                        </tr>
-                      </table></td>
-                    </tr>
-                  <tr>
-                    <td class="textogrande"><a href="historia/h02.pdf" target="_blank" class="botontexto">›› 31/05/76 CARTA DE AGRADECIMIENTO DE LA EMBAJADA DE AUSTRALIA</a></td>
-                    </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                        </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                        </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                        </tr>
-                      </table></td>
-                    </tr>
-                  <tr>
-                    <td class="textogrande"><a href="historia/h01.pdf" target="_blank" class="botontexto">›› 11/05/73 CARTA DE AGRADECIMIENTO DE LA EMBAJADA DE FRANCIA</a></td>
-                    </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                      </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                    </table></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><a href="historia/h08.pdf" target="_blank" class="botontexto">›› 20/03/76 CARTA DE AGRADECIMIENTO DE LA EMBAJADA DE MEXICO</a></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                      </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                    </table></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande">
-                      <a href="historia/h03.pdf" target="_blank" class="botontexto">›› 18/04/79 CARTA DE AGRADECIMIENTO DE LA EMBAJADA DE ITALIA </a></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                      </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                    </table></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><a href="historia/h06.pdf" target="_blank" class="botontexto">›› 26/09/88 CARTA DE AGRADECIMIENTO DE LA EMBAJADA DE JAPON</a></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                      <tr>
-                        <td bgcolor="#666666"><img src="img/pixel.gif" width="10" height="1" /></td>
-                      </tr>
-                      <tr>
-                        <td><img src="img/pixel.gif" width="10" height="10" /></td>
-                      </tr>
-                    </table></td>
-                  </tr>
-                  <tr>
-                    <td class="textogrande"><a href="historia/h04.pdf" target="_blank" class="botontexto">›› 11/08/00 CARTA DE AGRADECIMIENTO DE LA EMBAJADA COREA</a></td>
-                  </tr>
-                </table></td>
-                </tr>
-            </table></td>
-          </tr>
-        </table></td>
-        <td width="328"><img src="img/productos/historia.png" width="328" height="500" /></td>
-      </tr>
-    </table></td>
+        <td width="656" valign="top" background="img/back.png">
+          <table width="100%" border="0" cellspacing="0" cellpadding="30">
+            <td width="100%">  
+              
+              <a class="fancybox" rel="group" href="historia/full/h01-full.jpg" title="11/05/73 Carta de agradeciemiento de la embajada de Francia." ><img src="historia/thumb/h01-thumb.jpg" alt="" /></a>
+              <a class="fancybox" rel="group" href="historia/full/h02-full.jpg" title="31/05/76 Carta de agradeciemiento de la embajada de Australia." ><img src="historia/thumb/h02-thumb.jpg" alt="" /></a>
+              <a class="fancybox" rel="group" href="historia/full/h03-full.jpg" title="18/04/79 Carta de agradeciemiento de la embajada de Italia." ><img src="historia/thumb/h03-thumb.jpg" alt="" /></a>
+              <a class="fancybox" rel="group" href="historia/full/h04-full.jpg" title="11/08/00 Carta de agradeciemiento de la embajada de Corea." ><img src="historia/thumb/h04-thumb.jpg" alt="" /></a>
+              <a class="fancybox" rel="group" href="historia/full/h05-full.jpg" title="10/04/64 Carta de agradeciemiento de la embajada de España." ><img src="historia/thumb/h05-thumb.jpg" alt="" /></a>
+              <a class="fancybox" rel="group" href="historia/full/h06-full.jpg" title="26/09/88 Carta de agradeciemiento de la embajada de Japón." ><img src="historia/thumb/h06-thumb.jpg" alt="" /></a>
+              <a class="fancybox" rel="group" href="historia/full/h07-full.jpg" title="02/12/59 Carta de agradeciemiento de la embajada de EE.UU." ><img src="historia/thumb/h07-thumb.jpg" alt="" /></a>
+              <a class="fancybox" rel="group" href="historia/full/h08-full.jpg" title="20/03/76 Carta de agradeciemiento de la embajada de México." ><img src="historia/thumb/h08-thumb.jpg" alt="" /></a>
+              
+              <a class="fancybox" rel="group" href="prensa/full/p01-full.jpg" title="Nota de revista." ><img src="prensa/thumb/p01-thumb.jpg" alt="" /></a>
+              <a class="fancybox" rel="group" href="prensa/full/p02-full.jpg" title="Nota de revista." ><img src="prensa/thumb/p02-thumb.jpg" alt="" /></a>
+              <a class="fancybox" rel="group" href="prensa/full/p03-full.jpg" title="19/09/1978 Nota de revista." ><img src="prensa/thumb/p03-thumb.jpg" alt="" /></a>
+              <a class="fancybox" rel="group" href="prensa/full/p04-full.jpg" title="Flyer de muestra fotografica, Pinamar." ><img src="prensa/thumb/p04-thumb.jpg" alt="" /></a>
+              <a class="fancybox" rel="group" href="prensa/full/p05-full.jpg" title="Flyer. Los secretos del buen vestir, Audi." ><img src="prensa/thumb/p05-thumb.jpg" alt="" /></a>
+            </td>
+          </table>
+  </tr>
+  </table></td>
   </tr>
   <tr>
     <td height="75">&nbsp;</td>
